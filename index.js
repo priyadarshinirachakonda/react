192a9cbb723570c618cb05a229697de2664ec66c
priyadarshinirachakonda <158127844+priyadarshinirachakonda@users.noreply.github.com>
2025-09-13 15:28:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+    //   Write your code here.
+    <div>
+      <h1 className='heading'>Super Over Legue</h1>
+      <div className='img-cont'>
+        <img
+          src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
+          alt='rcb'
+        />
+        <img
+          src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
+          alt='csk'
+        />
+      </div>
+    </div>
+  )
+  
+  ReactDOM.render(element, document.getElementById('root'))
+  
